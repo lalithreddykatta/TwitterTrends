dfcd07b09e881ddf8d7db5ab5f57e5a05c50b823
Brandon Liu <brandonliu00@gmail.com>
2017-04-16 01:43:13 -0400

    Add files via upload

--- backend/Twendy/src/Tester.java
+++ backend/Twendy/src/Tester.java
@@ -12,7 +12,7 @@ public class Tester {
         try {
             TwitterCli.init();
             System.out.println("Initialized");
-            List<Tweet> tweet = TwitterCli.getTweetEfficiently("Rick and Morty");
+            List<Tweet> tweet = TwitterCli.getTweetEfficiently("Trump");
             System.out.println("Number of tweets: " + tweet.size());
             DataSorter sorter = new DataSorter(tweet);
 
@@ -20,27 +20,34 @@ public class Tester {
 
             PrintWriter out = new PrintWriter(new FileWriter("files/Data1.json", false));
 
-            JSONObject obj = new JSONObject();
+
 
 
 
 
             for (int i = 0; i < tweets.size(); i++){
                 //out.println((i + 1) + " " + tweets.get(i).size() + " 0.005");
-                obj.put("Hour" + (i + 1) + "TweetSize", tweets.get(i).size());
-                obj.put("Hour" + (i + 1) + "EmotionalRating", "0.005");
+                JSONObject obj = new JSONObject();
+                obj.put("time", (i + 1));
+
+                //obj.put("Hour" + (i + 1) + "TweetSize", tweets.get(i).size());
+                //obj.put("Hour" + (i + 1) + "EmotionalRating", "0.005");
                 JSONArray arrayJson = new JSONArray();
-                JSONObject item = new JSONObject();
+                //JSONObject item = new JSONObject();
                 for (int j = 0 ; j < tweets.get(i).size(); j++){
                     //out.println("    " + tweets.get(i).get(j).getStatus().getId() + "    " + tweets.get(i).get(j).getStatus().getText());
-                    item.put(Long.toString(tweets.get(i).get(j).getStatus().getId()), tweets.get(i).get(j).getStatus().getText());
-                    arrayJson.put(item);
+                    //item.put(Long.toString(tweets.get(i).get(j).getStatus().getId()), tweets.get(i).get(j).getStatus().getText());
+                    arrayJson.put(Long.toString(tweets.get(i).get(j).getStatus().getId()));
                 }
-                obj.put("Hour" + (i + 1) + "Tweets", arrayJson);
+                obj.put("tweets", arrayJson);
+                obj.put("emotionalrating", "2");
+                out.println(obj.toString());
             }
             //out.close();
             //System.out.print(obj.toString());
-            out.print(obj.toString());
+            //out.print(obj.toString());
+            //out.print("Test");
+            out.close();
         } catch (Exception e){
             System.out.println(e.getLocalizedMessage());
         }
